bd1d70847f7f3cb3e985ac2d93d03587d77bc4b1
Jan Ulferts <julferts@mesosphere.com>
2020-11-13 10:20:22 +0100

    Randomize checkin & checkout

--- covidify/test/visitgenerator/visit.go
+++ covidify/test/visitgenerator/visit.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/fatz/covidify/covidify/models"
 	"syreclabs.com/go/faker"
 )
 
 const MaxTableVisitors int = 6
+const DaysBack int = 10
+const MaxStayMinutes int = 180
+const MinStayMinutes int = 10
 
 func NewFakeVisit(table string) *models.Visit {
 	v := models.NewVisit()
 	v.TableNumber = table
 	numVisitors := 1
+
+	v.CheckIn = faker.Date().Backward(time.Duration(DaysBack) * 24 * time.Hour)
+
+	minCheckout := v.CheckIn.Add(time.Duration(MinStayMinutes) * time.Minute)
+	maxCheckout := v.CheckIn.Add(time.Duration(MaxStayMinutes) * time.Minute)
+	v.CheckOut = faker.Time().Between(minCheckout, maxCheckout)
 
 	if randNum := rand.Intn(MaxTableVisitors); randNum > 1 {
 		numVisitors = randNum
